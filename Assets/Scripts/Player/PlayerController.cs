1557c4c8585a3d5f26baf53fcf6f2c664ac8f082
Phan Thanh Duc <67327661+Deadworld-bit@users.noreply.github.com>
2025-05-19 09:48:41 +0700

    feat(Animation): Add all basic animation for character

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -12,22 +12,19 @@ public class PlayerController : MonoBehaviour
 
     private Vector3 _moveDirection = Vector3.zero;
     private bool _isMoving;
-    private int upperBodyLayerIndex; // Index of the UpperBodyLayer
-    private bool isGunOut = false;   // Tracks if the gun is out
+    private int _upperBodyLayerIndex;
+    private bool _isGunOut = false;
 
     private void Awake()
     {
-        _animator = GetComponent<Animator>();
-        _rigidbody = GetComponent<Rigidbody>();
-        _rigidbody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
-        // Get the layer index for UpperBodyLayer
-        upperBodyLayerIndex = _animator.GetLayerIndex("UpperBodyLayer");
+        CacheComponents();
+        SetRigidbodyConstraints();
+        GetUpperBodyLayerIndex();
     }
 
     private void Start()
     {
-        // Initially, upper body is controlled by the base layer
-        _animator.SetLayerWeight(upperBodyLayerIndex, 0f);
+        SetUpperBodyLayerWeight(0f);
     }
 
     private void Update()
@@ -42,12 +39,25 @@ public class PlayerController : MonoBehaviour
         HandleMovement();
     }
 
+    private void CacheComponents()
+    {
+        _animator = GetComponent<Animator>();
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void SetRigidbodyConstraints()
+    {
+        _rigidbody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+    }
+
+    private void GetUpperBodyLayerIndex()
+    {
+        _upperBodyLayerIndex = _animator.GetLayerIndex("UpperBodyLayer");
+    }
+
     private void HandleInput()
     {
-        // Existing movement input
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        Vector3 input = new Vector3(horizontal, 0f, vertical).normalized;
+        Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical")).normalized;
 
         if (input.magnitude > 0.1f)
         {
@@ -60,21 +70,23 @@ public class PlayerController : MonoBehaviour
             _isMoving = false;
         }
 
-        // Handle gun animation toggle with '1' key
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            if (!isGunOut)
-            {
-                // Gun is holstered, pull it out
-                isGunOut = true;
-                _animator.SetLayerWeight(upperBodyLayerIndex, 1f); // Enable upper body layer
-                _animator.SetTrigger("PullOut");                   // Trigger pull out animation
-            }
-            else
-            {
-                // Gun is out, hide it
-                _animator.SetTrigger("Hide");                      // Trigger hide animation
-            }
+            ToggleGun();
+        }
+    }
+
+    private void ToggleGun()
+    {
+        if (!_isGunOut)
+        {
+            _isGunOut = true;
+            SetUpperBodyLayerWeight(1f);
+            _animator.SetTrigger("PullOut");
+        }
+        else
+        {
+            _animator.SetTrigger("Hide");
         }
     }
 
@@ -91,11 +103,13 @@ public class PlayerController : MonoBehaviour
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         Plane groundPlane = new Plane(Vector3.up, transform.position);
+
         if (groundPlane.Raycast(ray, out float enter))
         {
             Vector3 mouseWorldPosition = ray.GetPoint(enter);
             Vector3 lookDirection = (mouseWorldPosition - transform.position).normalized;
             lookDirection.y = 0f;
+
             if (lookDirection.sqrMagnitude > 0.001f)
             {
                 Quaternion targetRotation = Quaternion.LookRotation(lookDirection);
@@ -112,10 +126,14 @@ public class PlayerController : MonoBehaviour
         _animator.SetFloat("Vertical", localMove.z);
     }
 
-    // Called by animation event at the end of the Hide animation
+    private void SetUpperBodyLayerWeight(float weight)
+    {
+        _animator.SetLayerWeight(_upperBodyLayerIndex, weight);
+    }
+
     public void OnHideAnimationFinished()
     {
-        isGunOut = false;
-        _animator.SetLayerWeight(upperBodyLayerIndex, 0f); // Disable upper body layer
+        _isGunOut = false;
+        SetUpperBodyLayerWeight(0f);
     }
 }
