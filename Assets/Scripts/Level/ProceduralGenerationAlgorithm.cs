c8829a6ef8dcc0bb35c76fb61762300f4ae3163d
Phan Thanh Duc <67327661+Deadworld-bit@users.noreply.github.com>
2025-05-21 16:36:50 +0700

    feat(map generator): Add map generator map using room first

--- Assets/Scripts/Level/ProceduralGenerationAlgorithm.cs
+++ Assets/Scripts/Level/ProceduralGenerationAlgorithm.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Collections.Generic;
-using UnityEditor.Experimental.GraphView;
+using Random = UnityEngine.Random;
+using System.Numerics;
 
 public static class ProceduralGenerationAlgorithm
 {
@@ -36,6 +37,74 @@ public static class ProceduralGenerationAlgorithm
         }
 
         return corridor;
+    }
+
+    public static List<BoundsInt> BinarySpacePartitioning(BoundsInt spaceToSplit, int minWidth, int minHeight)
+    {
+        Queue<BoundsInt> roomQueue = new Queue<BoundsInt>();
+        List<BoundsInt> roomList = new List<BoundsInt>();
+        roomQueue.Enqueue(spaceToSplit);
+        while (roomQueue.Count > 0)
+        {
+            var room = roomQueue.Dequeue();
+            if (room.size.y >= minHeight && room.size.x >= minWidth)
+            {
+                if (Random.value < 0.5f)
+                {
+                    if (room.size.y >= minHeight * 2)
+                    {
+                        SplitHorizontally(minHeight, roomQueue, room);
+                    }
+                    else if (room.size.x >= minWidth * 2)
+                    {
+                        SplitVertically(minWidth, roomQueue, room);
+                    }
+                    else if (room.size.x >= minWidth && room.size.y >= minHeight)
+                    {
+                        roomList.Add(room);
+                    }
+                }
+                else
+                {
+                    if (room.size.x >= minWidth * 2)
+                    {
+                        SplitVertically(minWidth, roomQueue, room);
+                    }
+                    else if (room.size.y >= minHeight * 2)
+                    {
+                        SplitHorizontally(minHeight, roomQueue, room);
+                    }
+                    else if (room.size.x >= minWidth && room.size.y >= minHeight)
+                    {
+                        roomList.Add(room);
+                    }
+                }
+            }
+        }
+
+        return roomList;
+    }
+
+    private static void SplitHorizontally(int minHeight, Queue<BoundsInt> roomQueue, BoundsInt room)
+    {
+        var ySplit = Random.Range(1, room.size.y);
+        BoundsInt firstRoom = new BoundsInt(room.min, new Vector3Int(room.size.x, ySplit, room.size.z));
+        BoundsInt secondRoom = new BoundsInt(new Vector3Int(room.min.x, room.min.y + ySplit, room.min.z),
+        new Vector3Int(room.size.x, room.size.y - ySplit, room.size.z));
+
+        roomQueue.Enqueue(firstRoom);
+        roomQueue.Enqueue(secondRoom);
+    }
+
+    private static void SplitVertically(int minWidth, Queue<BoundsInt> roomQueue, BoundsInt room)
+    {
+        var xSplit = Random.Range(1, room.size.x);
+        BoundsInt firstRoom = new BoundsInt(room.min, new Vector3Int(xSplit, room.size.y, room.size.z));
+        BoundsInt secondRoom = new BoundsInt(new Vector3Int(room.min.x + xSplit, room.min.y, room.min.z),
+        new Vector3Int(room.size.x - xSplit, room.size.y, room.size.z));
+
+        roomQueue.Enqueue(firstRoom);
+        roomQueue.Enqueue(secondRoom);
     }
 }
 
---
+++ Assets/Scripts/Level/RoomFirstDungeonGenerator.cs
@@ -0,0 +1,129 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class RoomFirstDungeonGenerator : RandomWalkGenerator
+{
+    [SerializeField] private int _dungeonWidth = 20;
+    [SerializeField] private int _dungeonHeight = 20;
+
+    [SerializeField] private int _minRoomWidth = 5;
+    [SerializeField] private int _minRoomHeight = 5;
+
+    [SerializeField][Range(0f, 10f)] private int _offset = 1;
+
+    [SerializeField] private bool _randomWalkRooms = false;
+
+    protected override void RunProceduralGeneration()
+    {
+        CreateRooms();
+    }
+
+    private void CreateRooms()
+    {
+        var roomList = ProceduralGenerationAlgorithm.BinarySpacePartitioning(new BoundsInt((Vector3Int)_startPosition,
+         new Vector3Int(_dungeonWidth, _dungeonHeight, 0)), _minRoomWidth, _minRoomHeight);
+
+        HashSet<Vector2Int> floor = new HashSet<Vector2Int>();
+        floor = CreateSimpleRooms(roomList);
+
+        List<Vector2Int> roomCenters = new List<Vector2Int>();
+        foreach (var room in roomList)
+        {
+            roomCenters.Add((Vector2Int)Vector3Int.RoundToInt(room.center));
+        }
+        HashSet<Vector2Int> corridors = ConnectRooms(roomCenters);
+        floor.UnionWith(corridors);
+
+        _floorVisualizer.InstantiateFloorTitles(floor);
+        WallGenerator.GenerateWalls(floor, _floorVisualizer);
+    }
+
+    private HashSet<Vector2Int> ConnectRooms(List<Vector2Int> roomCenters)
+    {
+        HashSet<Vector2Int> corridors = new HashSet<Vector2Int>();
+        var currentRoomCenter = roomCenters[Random.Range(0, roomCenters.Count)];
+        roomCenters.Remove(currentRoomCenter);
+
+        while (roomCenters.Count > 0)
+        {
+            Vector2Int closet = FindClosestPointTo(currentRoomCenter, roomCenters);
+            roomCenters.Remove(closet);
+            HashSet<Vector2Int> newCorridor = CreateCorridor(currentRoomCenter, closet);
+            currentRoomCenter = closet;
+            corridors.UnionWith(newCorridor);
+        }
+
+        return corridors;
+    }
+
+    private Vector2Int FindClosestPointTo(Vector2Int currentRoomCenter, List<Vector2Int> roomCenters)
+    {
+        Vector2Int closet = Vector2Int.zero;
+        float distance = float.MaxValue;
+
+        foreach (var position in roomCenters)
+        {
+            float currentDistance = Vector2.Distance(position, currentRoomCenter);
+            if (currentDistance < distance)
+            {
+                distance = currentDistance;
+                closet = position;
+            }
+        }
+        return closet;
+    }
+
+    private HashSet<Vector2Int> CreateCorridor(Vector2Int currentRoomCenter, Vector2Int destination)
+    {
+        HashSet<Vector2Int> corridor = new HashSet<Vector2Int>();
+        var position = currentRoomCenter;
+        corridor.Add(position);
+
+        while (position.y != destination.y)
+        {
+            if (destination.y > position.y)
+            {
+                position += Vector2Int.up;
+            }
+            else if (destination.y < position.y)
+            {
+                position += Vector2Int.down;
+            }
+            corridor.Add(position);
+        }
+        while (position.x != destination.x)
+        {
+            if (destination.x > position.x)
+            {
+                position += Vector2Int.right;
+            }
+            else if (destination.x < position.x)
+            {
+                position += Vector2Int.left;
+            }
+            corridor.Add(position);
+        }
+        
+        return corridor;
+    }
+
+    private HashSet<Vector2Int> CreateSimpleRooms(List<BoundsInt> roomList)
+    {
+        HashSet<Vector2Int> floor = new HashSet<Vector2Int>();
+        foreach (var room in roomList)
+        {
+            for (int col = _offset; col < room.size.x - _offset; col++)
+            {
+                for (int row = _offset; row < room.size.y - _offset; row++)
+                {
+                    Vector2Int position = (Vector2Int)room.min + new Vector2Int(col, row);
+                    floor.Add(position);
+                }
+            }
+        }
+
+        return floor;
+    }
+}
